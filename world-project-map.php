8633fbebe9b9824d5568a6d374557e48a7956278
Your Name <you@example.com>
2026-03-16 19:30:55 +0100

    final

--- world-project-map.php
+++ world-project-map.php
@@ -2,20 +2,22 @@
 /*
 Plugin Name: World Projects Interactive Map
 Description: Interactive map filtering posts by country taxonomy
-Version: 1.2
+Version: 1.3
 Author: Octave Benil
 */
 
-if (!defined('ABSPATH')) exit;
+if (!defined('ABSPATH'))
+    exit;
 
-function wpim_enqueue_assets(){
+function wpim_enqueue_assets()
+{
 
-    wp_enqueue_script('d3',"https://d3js.org/d3.v7.min.js",[],null,true);
-    wp_enqueue_script('topojson',"https://unpkg.com/topojson@3",[],null,true);
+    wp_enqueue_script('d3', "https://d3js.org/d3.v7.min.js", [], null, true);
+    wp_enqueue_script('topojson', "https://unpkg.com/topojson@3", [], null, true);
 
     wp_enqueue_script(
         'wpim-map',
-        plugin_dir_url(__FILE__).'js/map.js',
+        plugin_dir_url(__FILE__) . 'js/map.js',
         ['jquery'],
         '1.0',
         true
@@ -23,34 +25,35 @@ function wpim_enqueue_assets(){
 
     wp_enqueue_style(
         'wpim-style',
-        plugin_dir_url(__FILE__).'css/map.css'
+        plugin_dir_url(__FILE__) . 'css/map.css'
     );
 
-    wp_localize_script('wpim-map','WPIM',[
-        'ajax_url'=>admin_url('admin-ajax.php'),
-        'countries'=>wpim_get_countries()
+    wp_localize_script('wpim-map', 'WPIM', [
+        'ajax_url' => admin_url('admin-ajax.php'),
+        'countries' => wpim_get_countries()
     ]);
 
 }
 
-add_action('wp_enqueue_scripts','wpim_enqueue_assets');
+add_action('wp_enqueue_scripts', 'wpim_enqueue_assets');
 
 
-function wpim_get_countries(){
+function wpim_get_countries()
+{
 
     $terms = get_terms([
-        'taxonomy'=>'country',
-        'hide_empty'=>true
+        'taxonomy' => 'country',
+        'hide_empty' => true
     ]);
 
-    $data=[];
+    $data = [];
 
-    foreach($terms as $t){
+    foreach ($terms as $t) {
 
-        $data[]=[
-            'name'=>$t->name,
-            'slug'=>$t->slug,
-            'count'=>$t->count
+        $data[] = [
+            'name' => $t->name,
+            'slug' => $t->slug,
+            'count' => $t->count
         ];
 
     }
@@ -60,7 +63,8 @@ function wpim_get_countries(){
 }
 
 
-function wpim_shortcode(){
+function wpim_shortcode()
+{
 
     ob_start();
     ?>
@@ -74,56 +78,57 @@ function wpim_shortcode(){
 
 }
 
-add_shortcode('world_project_map','wpim_shortcode');
+add_shortcode('world_project_map', 'wpim_shortcode');
 
 
-function wpim_filter_projects(){
+function wpim_filter_projects()
+{
 
     $countries = isset($_POST['countries']) ? $_POST['countries'] : [];
     $paged = isset($_POST['page']) ? intval($_POST['page']) : 1;
 
     $args = [
-            'post_type'=>'post',
-            'posts_per_page'=>6,
-            'paged'=>$paged
+        'post_type' => 'post',
+        'posts_per_page' => 6,
+        'paged' => $paged
     ];
 
-    if(!empty($countries)){
+    if (!empty($countries)) {
 
-        $args['tax_query']=[
-                [
-                        'taxonomy'=>'country',
-                        'field'=>'slug',
-                        'terms'=>$countries
-                ]
+        $args['tax_query'] = [
+            [
+                'taxonomy' => 'country',
+                'field' => 'slug',
+                'terms' => $countries
+            ]
         ];
 
     }
 
     $query = new WP_Query($args);
 
-    if($query->have_posts()){
+    if ($query->have_posts()) {
 
         echo '<div class="wpim-project-grid">';
 
-        while($query->have_posts()){
+        while ($query->have_posts()) {
 
             $query->the_post();
 
-            $image = get_the_post_thumbnail_url(get_the_ID(),'large');
+            $image = get_the_post_thumbnail_url(get_the_ID(), 'large');
             $link = get_permalink();
             $date = get_the_date();
 
             echo '
 <div class="wpim-project-card">
-<a href="'.$link.'">
-<div class="wpim-image" style="background-image:url('.$image.')"></div>
+<a href="' . $link . '">
+<div class="wpim-image" style="background-image:url(' . $image . ')"></div>
 <div class="wpim-content">
-<h3>'.get_the_title().'</h3>
-<p>'.wp_trim_words(get_the_excerpt(),25).'</p>
+<h3>' . get_the_title() . '</h3>
+<p>' . wp_trim_words(get_the_excerpt(), 25) . '</p>
 <span class="wpim-more">EN SAVOIR PLUS »</span>
 </div>
-<div class="wpim-date">'.$date.'</div>
+<div class="wpim-date">' . $date . '</div>
 </a>
 </div>
 ';
@@ -137,13 +142,13 @@ function wpim_filter_projects(){
 
         $total_pages = $query->max_num_pages;
 
-        if($total_pages > 1){
+        if ($total_pages > 1) {
 
             echo '<div class="wpim-pagination">';
 
-            for($i=1;$i<=$total_pages;$i++){
+            for ($i = 1; $i <= $total_pages; $i++) {
 
-                echo '<span class="wpim-page" data-page="'.$i.'">'.$i.'</span>';
+                echo '<span class="wpim-page" data-page="' . $i . '">' . $i . '</span>';
 
             }
 
@@ -159,5 +164,5 @@ function wpim_filter_projects(){
 
 }
 
-add_action('wp_ajax_wpim_filter','wpim_filter_projects');
-add_action('wp_ajax_nopriv_wpim_filter','wpim_filter_projects');
+add_action('wp_ajax_wpim_filter', 'wpim_filter_projects');
+add_action('wp_ajax_nopriv_wpim_filter', 'wpim_filter_projects');
